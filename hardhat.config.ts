cde066249b9b1f5ac1ddfebab3da44a3d3ca086f
Zig Blathazar <42387185+ZigBalthazar@users.noreply.github.com>
2024-04-01 02:00:27 +0330

    fix: refactor (#6)

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -5,29 +5,82 @@ import { config as dotenvConfig } from "dotenv"
 import type { HardhatUserConfig } from "hardhat/config"
 import { resolve, join } from "path"
 
-const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || join(__dirname,".env")
+const dotenvConfigPath: string = process.env.DOTENV_CONFIG_PATH || join(__dirname, ".env")
 dotenvConfig({ path: resolve(__dirname, dotenvConfigPath) })
 
-const config: HardhatUserConfig = {
-	defaultNetwork: "hardhat",
-	solidity: "0.8.20",
-	networks: {
-		hardhat: {
-			allowUnlimitedContractSize: false,
+const etherscanApiKey = process.env.ETHERSCAN_API_KEY
+const account = process.env.PRIVATE_KEY
+const RPC = process.env.POLYGON_RPC_URL
+
+let config: HardhatUserConfig
+
+if (!process.env.CI) {
+	if (!etherscanApiKey) throw new Error("Hardhat_Config: etherscan api key is not defined.")
+	if (!account) throw new Error("Hardhat_Config: account is not defined.")
+	if (!RPC) throw new Error("Hardhat_Config: RPC is not defined.")
+
+	config = {
+		defaultNetwork: "hardhat",
+		solidity: "0.8.20",
+		networks: {
+			hardhat: {
+				allowUnlimitedContractSize: false,
+			},
+			mumbai: {
+				url: RPC,
+				accounts: [account],
+			},
 		},
-	},
-	typechain: {
-		outDir: "./contracts/evm/src/types",
-	},
-	mocha: {
-		timeout: 100000000,
-	},
-	paths: {
-		artifacts: "./contracts/evm/artifacts",
-		cache: "./contracts/evm/cache",
-		sources: "./contracts/evm/contracts",
-		tests: "./contracts/evm/test",
-	},
+		etherscan: {
+			apiKey: {
+				mumbai: etherscanApiKey,
+			},
+		},
+		gasReporter: {
+			currency: "USD",
+			enabled: true,
+			excludeContracts: [],
+			src: "./contracts",
+		},
+		typechain: {
+			outDir: "src/types",
+		},
+		mocha: {
+			timeout: 100000000,
+		},
+		paths: {
+			artifacts: "./artifacts",
+			cache: "./cache",
+			sources: "./contracts",
+		},
+	}
+} else {
+	config = {
+		defaultNetwork: "hardhat",
+		solidity: "0.8.20",
+		networks: {
+			hardhat: {
+				allowUnlimitedContractSize: false,
+			},
+		},
+		gasReporter: {
+			currency: "USD",
+			enabled: true,
+			excludeContracts: [],
+			src: "./contracts",
+		},
+		typechain: {
+			outDir: "src/types",
+		},
+		mocha: {
+			timeout: 100000000,
+		},
+		paths: {
+			artifacts: "./artifacts",
+			cache: "./cache",
+			sources: "./contracts",
+		},
+	}
 }
 
 export default config
